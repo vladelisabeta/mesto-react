1c203b9a703a27e63653c46c6f1e05ea7bc0a460
Elisaveta Vlad <vladelisabeta@yandex.ru>
2023-05-01 18:21:46 +0300

    starting open popup card

--- src/components/Card.js
+++ src/components/Card.js
@@ -5,17 +5,17 @@ function Card({ cards }) {
         <section class="place-grid">
             {cards.map(card => {
                 return (
-                    <div className="card">
-//                     <button type="button" className="card__trash"></button>
-//                     <img src={card.link} alt="загруженная картинка пользователя" className="card__image" />
-//                     <div className="card__heart-container">
-//                         <h2 className="card__title">{card.name}</h2>
-//                         <div className="card__like-container">
-//                             <button className="card__heart" type="button"></button>
-//                             <span className="card__like-count">{card.likes.length}</span>
-//                         </div>
-//                     </div>
-//                 </div>
+                    <div className="card" key={card._id}>
+                        <button type="button" className="card__trash"></button>
+                        <img src={card.link} alt={card.name} className="card__image" />
+                        <div className="card__heart-container">
+                            <h2 className="card__title">{card.name}</h2>
+                            <div className="card__like-container">
+                                <button className="card__heart" type="button"></button>
+                                <span className="card__like-count">{card.likes.length}</span>
+                            </div>
+                        </div>
+                    </div>
                 )
             })}
 
